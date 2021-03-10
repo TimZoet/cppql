c819e02f5600b0300893b45d5f4dd2d5de8cbe7a
Tim Zoet <ts.zoet@gmail.com>
2021-03-10 21:44:16 +0100

    Fixed blob column test

--- tests/cppql_test/src/get_column/get_column_blob.cpp
+++ tests/cppql_test/src/get_column/get_column_blob.cpp
@@ -2,9 +2,14 @@
 
 struct Foo
 {
-    int64_t a;
-    float   b;
-    int8_t  c;
+    int64_t a = 0;
+    float   b = 0;
+    int8_t  c = 0;
+};
+
+struct Bar
+{
+    int32_t a;
 };
 
 void GetColumnBlob::operator()()
@@ -22,13 +27,28 @@ void GetColumnBlob::operator()()
         compareTrue(stmt.step());
         compareTrue(stmt.reset());
 
-        std::vector<float> values2{40.5f, 50.5f, 60.5f};
-        compareTrue(stmt.bindTransientBlob(0, values2.data(), values2.size() * sizeof(float)));
+        std::vector<int64_t> values2{40, 50, 60};
+        compareTrue(stmt.bindTransientBlob(0, values2.data(), values2.size() * sizeof(int64_t)));
         compareTrue(stmt.step());
         compareTrue(stmt.reset());
 
-        std::vector<Foo> values3{{.a = 100, .b = 42, .c = 3}, {.a = 200, .b = 666, .c = -4}};
-        compareTrue(stmt.bindTransientBlob(0, values3.data(), values3.size() * sizeof(Foo)));
+        Foo value3 = {.a = 100, .b = 42, .c = 3};
+        compareTrue(stmt.bindTransientBlob(0, &value3, sizeof(Foo)));
+        compareTrue(stmt.step());
+        compareTrue(stmt.reset());
+
+        Foo value4 = {.a = 200, .b = 242, .c = 23};
+        compareTrue(stmt.bindTransientBlob(0, &value4, sizeof(Foo)));
+        compareTrue(stmt.step());
+        compareTrue(stmt.reset());
+
+        std::vector<Foo> values5 = {Foo{.a = 10, .b = 4.4f, .c = -4}, Foo{.a = -20, .b = -8.8f, .c = 5}};
+        compareTrue(stmt.bindTransientBlob(0, values5.data(), values5.size() * sizeof(Foo)));
+        compareTrue(stmt.step());
+        compareTrue(stmt.reset());
+
+         Foo value6 = {.a = 200, .b = 242, .c = 23};
+        compareTrue(stmt.bindTransientBlob(0, &value6, sizeof(Foo)));
         compareTrue(stmt.step());
         compareTrue(stmt.reset());
     });
@@ -36,32 +56,53 @@ void GetColumnBlob::operator()()
     // Create select statement.
     const auto stmt = db->createStatement("SELECT col1 FROM myTable;", true);
 
-    std::vector<float> values;
-
-    // Get row.
+    // Get row as vector of floats.
     compareTrue(stmt.step());
-    stmt.column(0, values);
-    compareEQ(values.size(), static_cast<size_t>(3));
-    compareEQ(values[0], 10.5f);
-    compareEQ(values[1], 20.5f);
-    compareEQ(values[2], 30.5f);
+    std::vector<float> vals1;
+    stmt.column(0, vals1);
+    compareEQ(vals1.size(), static_cast<size_t>(3));
+    compareEQ(vals1[0], 10.5f);
+    compareEQ(vals1[1], 20.5f);
+    compareEQ(vals1[2], 30.5f);
 
-    // Get row.
+    // Get row as vector of ints.
     compareTrue(stmt.step());
-    values = stmt.column<std::vector<float>>(0);
-    compareEQ(values.size(), static_cast<size_t>(3));
-    compareEQ(values[0], 40.5f);
-    compareEQ(values[1], 50.5f);
-    compareEQ(values[2], 60.5f);
+    const auto vals2 = stmt.column<std::vector<int64_t>>(0);
+    compareEQ(vals2.size(), static_cast<size_t>(3));
+    compareEQ(vals2[0], static_cast<int64_t>(40));
+    compareEQ(vals2[1], static_cast<int64_t>(50));
+    compareEQ(vals2[2], static_cast<int64_t>(60));
 
-    // Get row.
+    // Get row as struct.
     compareTrue(stmt.step());
-    auto values2 = stmt.column<std::vector<Foo>>(0);
-    compareEQ(values2.size(), static_cast<size_t>(2));
-    compareEQ(values2[0].a, 100);
-    compareEQ(values2[0].b, 42);
-    compareEQ(values2[0].c, 3);
-    compareEQ(values2[1].a, 200);
-    compareEQ(values2[1].b, 666);
-    compareEQ(values2[1].c, -4);
+    Foo val3;
+    stmt.column(0, val3);
+    compareEQ(val3.a, 100);
+    compareEQ(val3.b, 42);
+    compareEQ(val3.c, 3);
+
+    // Get row as struct.
+    compareTrue(stmt.step());
+    const auto val4 = stmt.column<Foo>(0);
+    compareEQ(val4.a, 200);
+    compareEQ(val4.b, 242);
+    compareEQ(val4.c, 23);
+
+    // Get row as vector of structs.
+    compareTrue(stmt.step());
+    const auto val5 = stmt.column<std::vector<Foo>>(0);
+    compareEQ(vals2.size(), static_cast<size_t>(3));
+    compareEQ(val5[0].a, static_cast<int64_t>(10));
+    compareEQ(val5[0].b, 4.4f);
+    compareEQ(val5[0].c, static_cast<int8_t>(-4));
+    compareEQ(val5[1].a, static_cast<int64_t>(-20));
+    compareEQ(val5[1].b, -8.8f);
+    compareEQ(val5[1].c, static_cast<int8_t>(5));
+
+    // Get row with wrong size.
+    compareTrue(stmt.step());
+    expectThrow([&stmt, this]() {
+        Bar val;
+        stmt.column(0, val);
+    });
 }
