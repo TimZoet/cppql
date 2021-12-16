c7474e1548c4ba41dfd9ce628136bb08d5f5e35c
Tim Zoet <ts.zoet@gmail.com>
2021-12-16 20:01:44 +0100

    Greatly improved packaging and conan integration

---
+++ conanfile.py
@@ -0,0 +1,93 @@
+from conans import ConanFile
+
+class CppqlConan(ConanFile):
+    ############################################################################
+    ## Package info.                                                          ##
+    ############################################################################
+    
+    name = "cppql"
+    
+    description = "C++ sqlite library."
+    
+    url = "https://github.com/TimZoet/cppql"
+
+    ############################################################################
+    ## Settings.                                                              ##
+    ############################################################################
+
+    python_requires = "pyreq/1.0.0@timzoet/stable"
+    
+    python_requires_extend = "pyreq.BaseConan"
+    
+    # Need this generator because sqlite doesn't work without it.
+    generators = "cmake_find_package",
+    
+    ############################################################################
+    ## Base methods.                                                          ##
+    ############################################################################
+    
+    def set_version(self):
+        base = self.python_requires["pyreq"].module.BaseConan
+        base.set_version(self, "modules/cppql/cppqlVersionString.cmake", "CPPQL_VERSION")
+    
+    def init(self):
+        base = self.python_requires["pyreq"].module.BaseConan
+        self.generators = base.generators + self.generators
+        self.settings = base.settings + self.settings
+        self.options = {**base.options, **self.options}
+        self.default_options = {**base.default_options, **self.default_options}
+    
+    ############################################################################
+    ## Building.                                                              ##
+    ############################################################################
+    
+    def export_sources(self):
+        self.copy("CMakeLists.txt")
+        self.copy("license")
+        self.copy("readme.md")
+        self.copy("cmake/*")
+        self.copy("modules/CMakeLists.txt")
+        self.copy("modules/cppql/*")
+    
+    def config_options(self):
+        base = self.python_requires["pyreq"].module.BaseConan
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+    
+    def requirements(self):
+        base = self.python_requires["pyreq"].module.BaseConan
+        base.requirements(self)
+        
+        self.requires("common/1.0.0@timzoet/stable")
+        self.requires("fmt/7.0.3")
+        self.requires("sqlite3/3.37.0")
+        
+        if self.options.build_tests:
+            self.requires("bettertest/1.0.0@timzoet/stable")
+    
+    def package_info(self):
+        self.cpp_info.libs = ["cppql"]
+    
+    def generate(self):
+        base = self.python_requires["pyreq"].module.BaseConan
+        
+        tc = base.generate_toolchain(self)
+        tc.generate()
+        
+        deps = base.generate_deps(self)
+        deps.generate()
+
+    def build(self):
+        base = self.python_requires["pyreq"].module.BaseConan
+        cmake = base.configure_cmake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        base = self.python_requires["pyreq"].module.BaseConan
+        cmake = base.configure_cmake(self)
+        cmake.install()
+
+
+
+    
--- external/hhdate/src/liboutput.cpp
+++
@@ -1,1 +0,0 @@
-// This file is here to make sure that VS outputs a LIB file; this does not happen when there is no .cpp file.
